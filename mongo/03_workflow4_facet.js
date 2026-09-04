b5843a6c10b34d0999564417466c2730517fd150
Nehal Gupta <nehalgupta2002@gmail.com>
2026-09-04 22:38:46 +0530

    Added final mongo execution stats

--- mongo/03_workflow4_facet.js
+++ mongo/03_workflow4_facet.js
@@ -1,46 +1,22 @@
 db.TripReviews.aggregate([
   {
     $match: {
-      created_at: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) }
+      rating: { $gte: 1 }
     }
   },
   {
     $facet: {
-      "rating_distribution": [
-        { 
-          $group: { 
-            _id: "$rating", 
-            count: { $sum: 1 } 
-          } 
-        },
+      rating_distribution: [
+        { $group: { _id: "$rating", count: { $sum: 1 } } },
         { $sort: { _id: 1 } }
       ],
-      "top_feedback_tags": [
+      driver_feedback_tags: [
         { $unwind: "$tags" },
-        { 
-          $group: { 
-            _id: "$tags", 
-            count: { $sum: 1 } 
-          } 
-        },
-        { $sort: { count: -1 } },
-        { $limit: 5 }
+        { $group: { _id: "$tags", count: { $sum: 1 } } },
+        { $sort: { count: -1 } }
       ],
-      "overall_summary": [
-        { 
-          $group: { 
-            _id: null, 
-            avg_rating: { $avg: "$rating" },
-            total_reviews: { $sum: 1 }
-          } 
-        },
-        {
-          $project: {
-            _id: 0,
-            avg_rating: { $round: ["$avg_rating", 2] },
-            total_reviews: 1
-          }
-        }
+      overall_average: [
+        { $group: { _id: null, avg_rating: { $avg: "$rating" } } }
       ]
     }
   }
